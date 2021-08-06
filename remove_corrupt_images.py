183068c133d51f56254aa056f197c2dd5c93e99c
acul <samsulrahmadani@gmail.com>
2021-08-07 04:18:11 +0700

    convert rgb

--- remove_corrupt_images.py
+++ remove_corrupt_images.py
@@ -4,8 +4,9 @@ from torchvision.io import read_image, ImageReadMode
 from tqdm import tqdm
 import psutil
 import ray
-
-path = "/home/user/data/CC12M/images"
+from PIL import Image
+import glob
+path = glob.glob("images/*/*.jpg")
 broken_path = "/home/user/data/CC12M/broken_images"
 num_cpus = psutil.cpu_count(logical=False)
 ray.init(num_cpus=num_cpus)
@@ -13,22 +14,23 @@ ray.init(num_cpus=num_cpus)
 
 @ray.remote
 def check_image(image_file):
-    image_path = os.path.join(path, image_file)
     try:
-        img = read_image(image_path, mode=ImageReadMode.RGB)
+        img = read_image(image_file, mode=ImageReadMode.RGB)
     except:
+        image = Image.open(image_file)
+        imga = image.convert('RGB')
+        imga.save(os.path.join(image_file))
         # shutil.move(image_path, broken_path)
-        os.remove(image_path)
 
 
-initial_count = len(os.listdir(path))
+initial_count = len(path)
 print(initial_count)
 # 10237845
 # 10237799
-for idx, image_file in enumerate(os.listdir(path)[::-1]):
+for idx, image_file in enumerate(path):
     check_image.remote(image_file)
     # check_image(image_file)
-    print(idx)
+    #print(idx)
 final_count = len(os.listdir(path))
 
 print(initial_count, final_count)
